ead189cbd80cea5e1d36cd0185715a5701369988
Rafa <ramijahsultana@gmail.com>
2026-03-30 21:47:57 +0600

    loops

---
+++ Problems_Set/Sheet 2 - Loops/test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int x, y;
+	    cin >> x >> y;
+	    
+	    int z = x/10;
+	    cout << z*y << "\n";
+	}
+}
