f4566a7093b6db8bde6bd50b4005a700d506e71c
Rafa <ramijahsultana@gmail.com>
2026-04-02 23:51:08 +0600

    Loops

---
+++ Problems_Set/Sheet 2 - Loops/A_Watermelon.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return 0;
+}
