828482316389b9a81a69f28e024755c83c0954f9
Rafa <ramijahsultana@gmail.com>
2026-04-04 22:33:49 +0600

    A no problems

--- Problems_Set/A_no/A_Bit.cpp
+++ Problems_Set/A_no/A_Bit.cpp
@@ -7,7 +7,26 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int t;
+    cin >> t;
+    int x = 0;
+    while(t--)
+    {
+        char a,b,c;
+        cin >> a >> b >> c;
+        if(a == '+' || b == '+' || c == '+')
+        {
+            x++;
+        }
+        else
+        {
+            x--;
+        }
+        if(t == 0)
+        {
+            cout << x << endl;
+        }
+    }
 
     return 0;
 }
