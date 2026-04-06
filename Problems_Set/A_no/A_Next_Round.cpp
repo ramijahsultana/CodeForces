424a2ef2c5ec8aa5643618f8099af2ce6070be2b
Rafa <ramijahsultana@gmail.com>
2026-04-06 23:13:34 +0600

    A no problems

--- Problems_Set/A_no/A_Next_Round.cpp
+++ Problems_Set/A_no/A_Next_Round.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define pb push_back
@@ -7,19 +7,25 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n,k;
+    int n, k;
     cin >> n >> k;
-    int cnt = 0;
-
-    for(int i=0; i<n; i++)
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
     {
-        int x;
-        cin >> x;
-        if(x > k && x > 0)
+        cin >> a[i];
+    }
+
+    int kth_score = a[k - 1]; 
+
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] >= kth_score && a[i] > 0)
         {
             cnt++;
         }
     }
+
     cout << cnt << endl;
 
     return 0;
