71f30f669a6acd84c3826a664b66855803c02922
Rafa <ramijahsultana@gmail.com>
2026-04-07 21:39:00 +0600

    A no problems

---
+++ Problems_Set/A_no/A_Domino_piling.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,m;
+    cin >> n >> m;
+
+    int ans = (n*m)/2;
+
+    cout << ans << "\n";
+
+    return 0;
+}
