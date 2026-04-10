d41983a8380ea7615e7aa552ad80beb17f8c6bbf
Rafa <ramijahsultana@gmail.com>
2026-04-10 22:44:35 +0600

    Loopd

---
+++ Problems_Set/Sheet 2 - Loops/N_Numbers_Histogram.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    char s;
+    cin >> s;
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        for (int i = 0; i < x; i++)
+        {
+            cout << s;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
