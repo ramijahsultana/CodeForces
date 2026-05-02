f402f4cd398b56e9cb66c36bc4ad45e6cb4d2ab1
Rafa <ramijahsultana@gmail.com>
2026-05-02 21:51:33 +0600

    A no

---
+++ Problems_Set/A_no/A_Elephant.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    
+
+    int ans = ceil(t/5.0);
+    cout << ans << endl;
+
+    return 0;
+}
