724d94a622735583fcf1aab6d87f6bc9966e188b
Rafa <ramijahsultana@gmail.com>
2026-05-26 23:23:10 +0600

    Array

--- Problems_Set/Sheet 3 - Array/P_Minimize_Number.cpp
+++ Problems_Set/Sheet 3 - Array/P_Minimize_Number.cpp
@@ -1,13 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define pb push_back
+
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    int n;
+    cin >> n;
 
-    
+    int ans = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+
+        int cnt = 0;
+
+        while (x % 2 == 0)
+        {
+            cnt++;
+            x /= 2;
+        }
+
+        ans = min(ans, cnt);
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
