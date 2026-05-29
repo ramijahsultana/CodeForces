0f633d177d4875ca78dfe4aac80cdf469493018d
Rafa <ramijahsultana@gmail.com>
2026-05-29 21:25:56 +0600

    Array

--- Problems_Set/Sheet 3 - Array/R_Permutation_with_arrays.cpp
+++ Problems_Set/Sheet 3 - Array/R_Permutation_with_arrays.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define pb push_back
@@ -7,7 +7,21 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    if (a == b)
+        cout << "yes\n";
+    else
+        cout << "no\n";
 
     return 0;
 }
