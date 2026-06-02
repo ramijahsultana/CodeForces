1a16142b1a131cd3678ed76b77c076f71f47ae91
Rafa <ramijahsultana@gmail.com>
2026-06-02 21:48:22 +0600

    900 Ratings

---
+++ Problems_Set/900 Ratings/B_Everything_Everywhere.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for(int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+        ll ans = 0;
+        for(int i=0; i<n-1; i++)
+        {
+            if(abs(a[i]-a[i+1]) == __gcd(a[i], a[i+1]))
+            {
+                ans++;
+            }
+        }
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
