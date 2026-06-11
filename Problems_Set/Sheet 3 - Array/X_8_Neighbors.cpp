cefe36d424cb21830efc8395f11e28c6826f3b91
Rafa <ramijahsultana@gmail.com>
2026-06-11 20:15:54 +0600

    Array

--- Problems_Set/Sheet 3 - Array/X_8_Neighbors.cpp
+++ Problems_Set/Sheet 3 - Array/X_8_Neighbors.cpp
@@ -1,13 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define pb push_back
+
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
 
-    
+    vector<string> a(n);
 
-    return 0;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int x, y;
+    cin >> x >> y;
+
+    x--;
+    y--;
+
+    int dx[] = {-1,-1,-1,0,0,1,1,1};
+    int dy[] = {-1,0,1,-1,1,-1,0,1};
+
+    for (int i = 0; i < 8; i++)
+    {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (nx >= 0 && nx < n && ny >= 0 && ny < m)
+        {
+            if (a[nx][ny] == '.')
+            {
+                cout << "no\n";
+                return 0;
+            }
+        }
+    }
+
+    cout << "yes\n";
 }
