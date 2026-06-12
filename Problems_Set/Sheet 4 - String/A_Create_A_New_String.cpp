98a3b0d07fe97e3f388e4e7743a1b0b0e99f278c
Rafa <ramijahsultana@gmail.com>
2026-06-12 09:29:51 +0600

    String

---
+++ Problems_Set/Sheet 4 - String/A_Create_A_New_String.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s, t;
+    cin >> s >> t;
+
+    cout << s.size() << " " << t.size() << "\n";
+    cout << s << " " << t << "\n";
+    return 0;
+}
