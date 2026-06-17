ef9b78c011f664cc91c6ce32226d0a0ab38468cc
Rafa <ramijahsultana@gmail.com>
2026-06-17 21:48:37 +0600

    String

---
+++ Problems_Set/Sheet 4 - String/F_Way_Too_Long_Words.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string s;
+        cin >> s;
+        if(s.size() <= 10)
+            cout << s << "\n";
+        else
+        {
+            cout << s[0] << s.size() - 2 << s[s.size() - 1] << "\n";
+        }
+    }
+
+    return 0;
+}
