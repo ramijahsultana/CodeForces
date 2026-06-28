1c72de50ac425ae86fde2b4eb6e92677b48c4729
Rafa <ramijahsultana@gmail.com>
2026-06-28 23:01:03 +0600

    String

---
+++ Problems_Set/Sheet 4 - String/O_Sort_string.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+
+    cout << s << "\n";
+
+    
+    return 0;
+}
