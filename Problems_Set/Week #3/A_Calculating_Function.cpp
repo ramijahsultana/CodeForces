2072de2bfbe325b0b4a192cba42a100260ad6d39
Rafa <ramijahsultana@gmail.com>
2026-08-20 21:01:38 +0600

    Week #3

---
+++ Problems_Set/Week #3/A_Calculating_Function.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+
+ll func(ll n)
+{
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    
+
+    return 0;
+}
