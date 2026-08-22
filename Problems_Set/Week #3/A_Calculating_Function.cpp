2be4526b73f1d355c90963f05acc3962bf6b0302
Rafa <ramijahsultana@gmail.com>
2026-08-22 20:13:31 +0600

    week #3

--- Problems_Set/Week #3/A_Calculating_Function.cpp
+++ Problems_Set/Week #3/A_Calculating_Function.cpp
@@ -1,21 +1,33 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define pb push_back
+#include <iostream>
 
-ll func(ll n)
-{
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
     
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        int rating;
+        cin >> rating;
+        if (rating >= 1000) {
+            count++;
+        }
+    }
+    
+    cout << count << "\n";
 }
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main() {
 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int t;
     cin >> t;
+    while (t--) {
+        solve();
+    }
     
-
     return 0;
 }
