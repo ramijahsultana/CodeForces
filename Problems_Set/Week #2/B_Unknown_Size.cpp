c97cff1eab8989917e50139ef1161d90d441fdb5
Rafa <ramijahsultana@gmail.com>
2026-08-26 20:05:15 +0600

    Week #2

--- Problems_Set/Week #2/B_Unknown_Size.cpp
+++ Problems_Set/Week #2/B_Unknown_Size.cpp
@@ -28,3 +28,27 @@ int main()
 
     return 0;
 }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int t;
+// 	cin >> t;
+// 	while(t--)
+// 	{
+// 	    int n;
+// 	    cin >> n;
+// 	    while(n>10)
+// 	    {
+// 	        if(n%10 != 0)
+// 	            cout << n%10;
+// 	        n = n/10;
+// 	    }
+// 	    cout << n << endl;
+// 	}
+	
+	
+
+//}
